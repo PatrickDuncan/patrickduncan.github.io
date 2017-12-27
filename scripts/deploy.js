14498840c6f89d78bd90b1ca9448f8aec3ccedd8
Patrick Diamond Duncan <patrick.duncan@outlook.com>
2017-12-27 01:36:58 -0500

    Added comments and a checkout branch method

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,15 +4,31 @@ const chalk = require('chalk');
 const { exec } = require('child_process');
 const rimraf = require('rimraf');
 
-// Takes in a shell command and executes it as you would expect
+/*
+* Runs a command in a new shell in the repo's root directory
+* @param {String} command - Which command to run
+* @return {String} - The standard output
+*/
 const SHELL = command => {
   return execSync(command).toString('utf8');
 }
 
-const get_master_checkout_options = () => {
+/*
+* @param {String} branch - Which branch to check
+* @return {String} - -b if the branch doesn't exist
+*/
+const get_master_checkout_options = branch => {
   return SHELL('git branch')
          .split('\n')
          .map(s => s.trim())
-         .indexOf('master')
+         .indexOf(branch)
          === -1 ? "-b" : ""
+}
+
+/*
+* Git checkouts a branch
+* @param {String} branch - The branch to checkout
+*/
+const checkout_branch = branch => {
+  SHELL(`git checkout ${get_master_checkout_options(branch)} ${branch}`);
 }
