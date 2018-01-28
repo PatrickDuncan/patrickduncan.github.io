b5294b73fb6e1935251397d776ea5a0b5e3dff35
Patrick Diamond Duncan <patrick.duncan@outlook.com>
2018-01-28 05:53:04 -0500

    Fleshed out App tests

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import App from '../App';
+import sinon from 'sinon';
 
-it('renders without crashing', () => {
-  shallow(<App />);
+import App from './../App';
+import Header from './../components/common/Header';
+import Navbar from './../components/Navbar';
+import Home from './../components/Home';
+import Work from './../components/Work';
+import Projects from './../components/Projects';
+import Skills from './../components/Skills';
+import Education from './../components/Education';
+import About from './../components/About';
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('renders the home section', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Home)).to.have.length(1);
+  });
+
+  it('renders the work section', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Work)).to.have.length(1);
+  });
+
+  it('renders the projects section', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Projects)).to.have.length(1);
+  });
+
+  it('renders the skills section', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Skills)).to.have.length(1);
+  });
+
+  it('renders the education section', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Education)).to.have.length(1);
+  });
+
+  it('renders the about section', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(About)).to.have.length(1);
+  });
+
+  it('renders the navbar', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Navbar)).to.have.length(1);
+  });
+
+  it('renders a header between each section', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header)).to.have.length(5);
+  });
 });
